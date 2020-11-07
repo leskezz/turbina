1ad74ca6a17d5a550c1f395d6ab90cacf8060e0b
Aleksey Kozlovskiy <aikozlovsky@yandex.ru>
2020-11-07 11:56:05 +1000

    feat: transform project into React using CRA

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Header(props) {
+
+    return (
+        <header className="header">
+            <div className="header__topbar">
+                <a href="#" className="header__logo hover-opacity"><img src={props.logo} alt="Логотип" /></a>
+                <ul className="header__links">
+                    <li><a href="#" className="header__link">Стриминги</a></li>
+                    <li><a href="#" className="header__link">Яндекс.Музыка &#8599;</a></li>
+                    <li><a href="#" className="header__link">Spotify &#8599;</a></li>
+                    <li><a href="#" className="header__link">Apple Music &#8599;</a></li>
+                    <li><a href="#" className="header__link">VK Music &#8599;</a></li>
+                </ul>
+            </div>
+            <h1 className="header__title"><img className="header__title-img" src={props.imageTitle} alt="Турбина" /></h1>
+            <div className="audioplayer"></div>
+        </header>
+    )
+}
+
+export default Header;
