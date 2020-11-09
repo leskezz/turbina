7cf47722c38cacb84553963364b8c1940780162c
Melty <Meltygd@gmail.com>
2020-11-09 16:50:34 +0900

    Сохранение незнаю чего, гит не показывает

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import FormValidation from './components/FormValidation.js';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +11,10 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+const validation = new FormValidation();
+validation.enableValidation();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
