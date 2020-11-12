e5a4186efc4e312a3ade001449461b897303115c
Aleksey Kozlovskiy <aikozlovsky@yandex.ru>
2020-11-12 22:58:54 +1000

    feat: Добавь фичу убирать и показывать ссылки на стриминговые сервисы

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import Audioplayer from './Audioplayer/Audioplayer';
+import closeStreamings from '../images/header-close-streamings.svg';
 
 function Header(props) {
+
+    const [isStreamingsOpen, setStreamingOpen] = React.useState(false);
+
+    function handleStreamingButtonClick() {
+        setStreamingOpen(!isStreamingsOpen);
+    };
 
     return (
         <header className="header">
             <div className="header__topbar">
-                <a href="#" className="header__logo hover-opacity"><img src={props.logo} alt="Логотип" /></a>
+                <a href="https://marshakbooks.ru/" className="header__logo hover-opacity"><img src={props.logo} alt="Логотип" /></a>
                 <div className="header__streaming">
-                    <button type="button" className="header__link header__streaming-button">Стриминги</button>
-                <ul className="header__links">
-                    <li><a href="#" className="header__link">Яндекс.Музыка &#8599;</a></li>
-                    <li><a href="#" className="header__link">Spotify &#8599;</a></li>
-                    <li><a href="#" className="header__link">Apple Music &#8599;</a></li>
-                    <li><a href="#" className="header__link">VK Music &#8599;</a></li>
+                    <button type="button" className={`header__streaming-button ${isStreamingsOpen && 'header__streaming-button_clicked'}`} onClick={handleStreamingButtonClick}>{!isStreamingsOpen ? 'Стриминги' : (<img src={closeStreamings} alt="Закрыть" className="header__streaming-close"/>)}</button>
+                <ul className={`header__links ${!isStreamingsOpen && 'header__links_disable'}`}>
+                    <li><a href="https://music.yandex.ru/home" className="header__link">Яндекс.Музыка &#8599;</a></li>
+                    <li><a href="https://www.spotify.com/" className="header__link">Spotify &#8599;</a></li>
+                    <li><a href="https://music.apple.com/" className="header__link">Apple Music &#8599;</a></li>
+                    <li><a href="https://vk.com/vkmusic" className="header__link">VK Music &#8599;</a></li>
                 </ul>
                 </div>
             </div>
